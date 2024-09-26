c3c80aacd3c9b11b661c4bba5a71a5e5b6954e91
shinikatame <shinikatame@gmail.com>
2024-09-26 16:12:51 -0300

    start backend

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from importlib import import_module
+from os import listdir
+
+app = FastAPI(docs_url = '/')
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
+
+def load(path = 'routers'): 
+    dir = listdir(path)
+    dir.sort()
+
+    for file in dir:
+        if not file.startswith('_'):
+            if file.endswith('.py'):
+                module = import_module(f'{path}.{file}'.replace('.py', '').replace('/', '.'))
+                app.include_router(module.router)
+
+            else:
+                load(path + '/' + file)
+
+load()
+
+if __name__ == '__main__':
+    from uvicorn import run
+    run(app, host = '0.0.0.0', port = 8000)
